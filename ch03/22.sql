1b49bfbe7f6b034d653b33941ce7ee9c5a0a7bfc
zzsza <zzsza@naver.com>
2020-11-13 19:38:05 +0900

    [modify] col_name

--- ch03/22.sql
+++ ch03/22.sql
@@ -6,8 +6,8 @@ WITH example AS (
 SELECT 
   city
   , CHAR_LENGTH(city) AS char_len
-  , TO_CODE_POINTS(city)[OFFSET(1)] AS first_code_point
-  , ARRAY_LENGTH(TO_CODE_POINTS(city)) AS num_code_points
+  , TO_CODE_POINTS(city)[OFFSET(1)] AS first_codept
+  , ARRAY_LENGTH(TO_CODE_POINTS(city)) AS num_codept
   , CAST (city AS BYTES) AS bytes
   , BYTE_LENGTH(city) AS byte_len
 FROM example
